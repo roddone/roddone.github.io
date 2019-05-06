a0350614ad5d9a47c24e1dad8a1b287967b81e39
Romain ODDONE <roddone@publicis-eto.fr>
2019-05-06 17:55:01 +0200

    start handlebars refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
 const autoprefixer = require("gulp-autoprefixer");
 const browsersync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
+const fs = require('fs');
 const del = require("del");
 const gulp = require("gulp");
 const header = require("gulp-header");
@@ -12,120 +13,149 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const handlebars = require("gulp-compile-handlebars");
 
 // Load package.json for banner
 const pkg = require('./package.json');
 
 // Set the banner content
 const banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
+	' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+	' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+	' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
+	' */\n',
+	'\n'
 ].join('');
 
 // BrowserSync
 function browserSync(done) {
-  browsersync.init({
-    server: {
-      baseDir: "./"
-    },
-    port: 3000
-  });
-  done();
+	browsersync.init({
+		server: {
+			baseDir: "./dist"
+		},
+		port: 3000
+	});
+	done();
 }
 
 // BrowserSync reload
 function browserSyncReload(done) {
-  browsersync.reload();
-  done();
+	browsersync.reload();
+	done();
 }
 
 // Clean vendor
 function clean() {
-  return del(["./vendor/"]);
+	return del(["./vendor/", './dist/']);
 }
 
 // Bring third party dependencies from node_modules into vendor directory
 function modules() {
-  // Bootstrap
-  var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
-  // Font Awesome
-  var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
-    .pipe(gulp.dest('./vendor'));
-  // jQuery Easing
-  var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-    .pipe(gulp.dest('./vendor/jquery-easing'));
-  // jQuery
-  var jquery = gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'));
-  return merge(bootstrap, fontAwesome, jquery, jqueryEasing);
+	// Bootstrap
+	var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
+		.pipe(gulp.dest('./dist/vendor/bootstrap'));
+	// Font Awesome
+	var fontAwesome = gulp.src('./node_modules/@fortawesome/**/*')
+		.pipe(gulp.dest('./dist/vendor'));
+	// jQuery Easing
+	var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
+		.pipe(gulp.dest('./dist/vendor/jquery-easing'));
+	// jQuery
+	var jquery = gulp.src([
+		'./node_modules/jquery/dist/*',
+		'!./node_modules/jquery/dist/core.js'
+	])
+		.pipe(gulp.dest('./dist/vendor/jquery'));
+	return merge(bootstrap, fontAwesome, jquery, jqueryEasing);
 }
 
 // CSS task
 function css() {
-  return gulp
-    .src("./scss/**/*.scss")
-    .pipe(plumber())
-    .pipe(sass({
-      outputStyle: "expanded",
-      includePaths: "./node_modules",
-    }))
-    .on("error", sass.logError)
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest("./css"))
-    .pipe(rename({
-      suffix: ".min"
-    }))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("./css"))
-    .pipe(browsersync.stream());
+	return gulp
+		.src("./scss/**/*.scss")
+		.pipe(plumber())
+		.pipe(sass({
+			outputStyle: "expanded",
+			includePaths: "./node_modules",
+		}))
+		.on("error", sass.logError)
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(header(banner, {
+			pkg: pkg
+		}))
+		.pipe(gulp.dest("./dist/css"))
+		.pipe(rename({
+			suffix: ".min"
+		}))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest("./dist/css"))
+		.pipe(browsersync.stream());
 }
 
 // JS task
 function js() {
-  return gulp
-    .src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browsersync.stream());
+	return gulp
+		.src([
+			'./js/*.js',
+			'!./js/*.min.js'
+		])
+		.pipe(uglify())
+		.pipe(header(banner, {
+			pkg: pkg
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./dist/js'))
+		.pipe(browsersync.stream());
+}
+
+function img() {
+	return gulp
+		.src(['./img/*'])
+		.pipe(gulp.dest('./dist/img'))
+		.pipe(browsersync.stream());
+}
+
+function html() {
+	var english = gulp.src('index.hbs')
+		.pipe(handlebars(JSON.parse(fs.readFileSync('./lang/en.json')), {}))
+		.pipe(rename('english.html'))
+		.pipe(gulp.dest('./dist'));
+
+	var french = gulp.src('index.hbs')
+		.pipe(handlebars(JSON.parse(fs.readFileSync('./lang/fr.json')), {}))
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('./dist'));
+
+	return merge(english, french)
+		.pipe(browsersync.stream());
+
 }
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./scss/**/*", css);
-  gulp.watch("./js/**/*", js);
-  gulp.watch("./**/*.html", browserSyncReload);
+	gulp.watch("./scss/**/*", css);
+	gulp.watch("./js/**/*", js);
+	gulp.watch("./img/**/*", img);
+	gulp.watch("./**/*.html", browserSyncReload);
+	gulp.watch("./lang/**/*.json", html);
+	gulp.watch("./**/*.hbs", html);
 }
 
 // Define complex tasks
 const vendor = gulp.series(clean, modules);
-const build = gulp.series(vendor, gulp.parallel(css, js));
+const build = gulp.series(vendor, gulp.parallel(html, img, css, js));
 const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 
 // Export tasks
 exports.css = css;
 exports.js = js;
+exports.img = img;
+exports.html = html;
 exports.clean = clean;
 exports.vendor = vendor;
 exports.build = build;
